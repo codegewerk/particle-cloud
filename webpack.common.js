df7651176c10a12d2ab4620879d0351a09cd21be
Felix Bernhardt <felix.bernhardt@mailbox.org>
2020-09-02 14:42:39 +0200

    update webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,10 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
   entry: path.resolve(__dirname, "src", "index.js"),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "particles.js",
     library: "particles",
   },
-  devtool: "source-map",
 };

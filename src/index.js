2af70bf16ce3c90598d010845f86938a831e7e93
Felix Bernhardt <felix.bernhardt@mailbox.org>
2020-09-02 11:08:00 +0200

    restructure index.js

--- src/index.js
+++ src/index.js
@@ -25,18 +25,77 @@ class Particles {
       );
     }
 
-    this.scheduler = getAnimationFunctions();
-    this.animate = this.animate.bind(this);
+    this.scheduler = getAnimationScheduler();
 
     this.breakpoints = [];
     this.breakpointSettings = [];
 
-    this._initializeCanvas();
-    this._initializeEvents();
-    this._registerBreakpoints();
-    this._checkResponsive();
+    this.onResize = () => this.resize();
 
-    this._initializeStorage();
+    this.initializeCanvas();
+    this.registerBreakpoints();
+
+    this.scaleContext();
+    this._checkResponsive();
+    this.initializeParticleField();
+
+    this.subscribeToEvents();
+  }
+
+  initializeCanvas() {
+    this.element = document.querySelector(this.options.selector);
+    this.element.style.width = "100%";
+    this.element.style.height = "100%";
+
+    this.context = this.element.getContext("2d");
+    this.ratio = getPixelRatio(this.context);
+  }
+
+  registerBreakpoints() {
+    var _ = this,
+      breakpoint,
+      currentBreakpoint,
+      l,
+      responsiveSettings = _.options.responsive || null;
+
+    if (
+      typeof responsiveSettings === "object" &&
+      responsiveSettings !== null &&
+      responsiveSettings.length
+    ) {
+      for (breakpoint in responsiveSettings) {
+        l = _.breakpoints.length - 1;
+        currentBreakpoint = responsiveSettings[breakpoint].breakpoint;
+
+        if (responsiveSettings.hasOwnProperty(breakpoint)) {
+          while (l >= 0) {
+            if (_.breakpoints[l] && _.breakpoints[l] === currentBreakpoint) {
+              _.breakpoints.splice(l, 1);
+            }
+
+            l--;
+          }
+
+          _.breakpoints.push(currentBreakpoint);
+          _.breakpointSettings[currentBreakpoint] =
+            responsiveSettings[breakpoint].options;
+        }
+      }
+
+      _.breakpoints.sort(function (a, b) {
+        return b - a;
+      });
+    }
+  }
+
+  initializeParticleField() {
+    const [clientWidth, clientHeight] = this.getDimensions();
+    this.particleField = new ParticleField(
+      this.options,
+      this.context,
+      clientWidth,
+      clientHeight
+    );
   }
 
   start() {
@@ -54,7 +113,7 @@ class Particles {
 
   animate() {
     this.step();
-    this._animation = this.scheduler.requestFrame(this.animate);
+    this._animation = this.scheduler.requestFrame(() => this.animate());
   }
 
   step() {
@@ -72,169 +131,90 @@ class Particles {
   }
 
   destroy() {
-    window.removeEventListener("resize", this.listener, false);
+    this.unsubscribeFromEvents();
     this.stop();
   }
-}
 
-Particles.prototype._initializeCanvas = function () {
-  this.element = document.querySelector(this.options.selector);
-  this.element.style.width = "100%";
-  this.element.style.height = "100%";
-
-  this.context = this.element.getContext("2d");
-
-  this.ratio = getPixelRatio(this.context);
-  this.setContextScale();
-};
-
-Particles.prototype.setContextScale = function () {
-  const [clientWidth, clientHeight] = this.getDimensions();
-  this.element.width = clientWidth * this.ratio;
-  this.element.height = clientHeight * this.ratio;
-
-  this.context.scale(this.ratio, this.ratio);
-};
-
-Particles.prototype._resize = function () {
-  this.setContextScale();
-
-  clearTimeout(this.windowDelay);
-  this.windowDelay = window.setTimeout(() => {
-    this._checkResponsive();
-    this._refresh();
-  }, 50);
-};
-
-function getPixelRatio(context) {
-  const devicePixelRatio = window.devicePixelRatio || 1;
-  const backingStoreRatio =
-    context.webkitBackingStorePixelRatio ||
-    context.mozBackingStorePixelRatio ||
-    context.msBackingStorePixelRatio ||
-    context.oBackingStorePixelRatio ||
-    context.backingStorePixelRatio ||
-    1;
-
-  return devicePixelRatio / backingStoreRatio;
-}
-
-Particles.prototype._initializeEvents = function () {
-  var _ = this;
-
-  _.listener = function () {
-    _._resize();
-  }.bind(this);
-  window.addEventListener("resize", _.listener, false);
-};
-
-Particles.prototype._initializeStorage = function () {
-  const [clientWidth, clientHeight] = this.getDimensions();
-  this.particleField = new ParticleField(
-    this.options,
-    this.context,
-    clientWidth,
-    clientHeight
-  );
-};
-
-Particles.prototype.getDimensions = function () {
-  const canvas = this.element;
-
-  let clientHeight;
-  if (canvas.offsetParent && canvas.offsetParent.nodeName === "BODY") {
-    clientHeight = window.innerHeight;
-  } else {
-    clientHeight = canvas.offsetParent
-      ? canvas.offsetParent.clientHeight
-      : canvas.clientHeight;
+  subscribeToEvents() {
+    window.addEventListener("resize", this.onResize, false);
   }
 
-  const clientWidth = canvas.offsetParent
-    ? canvas.offsetParent.clientWidth
-    : canvas.clientWidth;
-
-  return [clientWidth, clientHeight];
-};
-
-Particles.prototype._registerBreakpoints = function () {
-  var _ = this,
-    breakpoint,
-    currentBreakpoint,
-    l,
-    responsiveSettings = _.options.responsive || null;
-
-  if (
-    typeof responsiveSettings === "object" &&
-    responsiveSettings !== null &&
-    responsiveSettings.length
-  ) {
-    for (breakpoint in responsiveSettings) {
-      l = _.breakpoints.length - 1;
-      currentBreakpoint = responsiveSettings[breakpoint].breakpoint;
-
-      if (responsiveSettings.hasOwnProperty(breakpoint)) {
-        while (l >= 0) {
-          if (_.breakpoints[l] && _.breakpoints[l] === currentBreakpoint) {
-            _.breakpoints.splice(l, 1);
-          }
-
-          l--;
-        }
-
-        _.breakpoints.push(currentBreakpoint);
-        _.breakpointSettings[currentBreakpoint] =
-          responsiveSettings[breakpoint].options;
-      }
-    }
-
-    _.breakpoints.sort(function (a, b) {
-      return b - a;
-    });
+  unsubscribeFromEvents() {
+    window.removeEventListener("resize", this.onResize, false);
   }
-};
 
-Particles.prototype._checkResponsive = function () {
-  var _ = this,
-    breakpoint,
-    targetBreakpoint = false,
-    windowWidth = window.innerWidth;
+  resize() {
+    this.scaleContext();
 
-  if (
-    _.options.responsive &&
-    _.options.responsive.length &&
-    _.options.responsive !== null
-  ) {
-    targetBreakpoint = null;
+    clearTimeout(this.windowDelay);
+    this.windowDelay = window.setTimeout(() => {
+      this._checkResponsive();
+      this.initializeParticleField();
+    }, 50);
+  }
 
-    for (breakpoint in _.breakpoints) {
-      if (_.breakpoints.hasOwnProperty(breakpoint)) {
-        if (windowWidth <= _.breakpoints[breakpoint]) {
-          targetBreakpoint = _.breakpoints[breakpoint];
-        }
-      }
-    }
+  scaleContext() {
+    const [clientWidth, clientHeight] = this.getDimensions();
+    this.element.width = clientWidth * this.ratio;
+    this.element.height = clientHeight * this.ratio;
 
-    if (targetBreakpoint !== null) {
-      _.activeBreakpoint = targetBreakpoint;
-      _.options = extend(_.options, _.breakpointSettings[targetBreakpoint]);
+    this.context.scale(this.ratio, this.ratio);
+  }
+
+  getDimensions() {
+    const canvas = this.element;
+
+    let clientHeight;
+    if (canvas.offsetParent && canvas.offsetParent.nodeName === "BODY") {
+      clientHeight = window.innerHeight;
     } else {
-      if (_.activeBreakpoint !== null) {
-        _.activeBreakpoint = null;
-        targetBreakpoint = null;
+      clientHeight = canvas.offsetParent
+        ? canvas.offsetParent.clientHeight
+        : canvas.clientHeight;
+    }
 
-        _.options = extend(_.options, _.originalSettings);
+    const clientWidth = canvas.offsetParent
+      ? canvas.offsetParent.clientWidth
+      : canvas.clientWidth;
+
+    return [clientWidth, clientHeight];
+  }
+
+  _checkResponsive() {
+    var _ = this,
+      breakpoint,
+      targetBreakpoint = false,
+      windowWidth = window.innerWidth;
+
+    if (
+      _.options.responsive &&
+      _.options.responsive.length &&
+      _.options.responsive !== null
+    ) {
+      targetBreakpoint = null;
+
+      for (breakpoint in _.breakpoints) {
+        if (_.breakpoints.hasOwnProperty(breakpoint)) {
+          if (windowWidth <= _.breakpoints[breakpoint]) {
+            targetBreakpoint = _.breakpoints[breakpoint];
+          }
+        }
+      }
+
+      if (targetBreakpoint !== null) {
+        _.activeBreakpoint = targetBreakpoint;
+        _.options = extend(_.options, _.breakpointSettings[targetBreakpoint]);
+      } else {
+        if (_.activeBreakpoint !== null) {
+          _.activeBreakpoint = null;
+          targetBreakpoint = null;
+
+          _.options = extend(_.options, _.originalSettings);
+        }
       }
     }
   }
-};
-
-Particles.prototype._refresh = function () {
-  var _ = this;
-
-  _._initializeStorage();
-  _.draw();
-};
+}
 
 function extend(source, obj) {
   Object.keys(obj).forEach(function (key) {
@@ -244,7 +224,7 @@ function extend(source, obj) {
   return source;
 }
 
-function getAnimationFunctions() {
+function getAnimationScheduler() {
   let requestFrame =
     window.requestAnimationFrame ||
     window.webkitRequestAnimationFrame ||
@@ -262,6 +242,19 @@ function getAnimationFunctions() {
   }
 
   return { requestFrame, cancelFrame };
+}
+
+function getPixelRatio(context) {
+  const devicePixelRatio = window.devicePixelRatio || 1;
+  const backingStoreRatio =
+    context.webkitBackingStorePixelRatio ||
+    context.mozBackingStorePixelRatio ||
+    context.msBackingStorePixelRatio ||
+    context.oBackingStorePixelRatio ||
+    context.backingStorePixelRatio ||
+    1;
+
+  return devicePixelRatio / backingStoreRatio;
 }
 
 export { Particle, Particles, hex2rgb };

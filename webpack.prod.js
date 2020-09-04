f9cf70e034425968eb7f83ebc41fb166a988d6c9
Felix Bernhardt <felix.bernhardt@mailbox.org>
2020-09-04 16:32:09 +0200

    create multiple bundles

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,25 @@
+const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
-module.exports = merge(common, {
-  mode: "production",
-  output: {
-    filename: "particles.min.js",
-  },
-});
+module.exports = [
+  merge(common, {
+    mode: "production",
+    devtool: "source-map",
+    output: {
+      filename: "particles.min.js",
+      libraryTarget: "window",
+    },
+  }),
+  merge(common, {
+    mode: "production",
+    devtool: "source-map",
+    output: {
+      filename: "particles.js",
+      libraryTarget: "commonjs2",
+    },
+    optimization: {
+      minimize: false,
+    },
+  }),
+];
